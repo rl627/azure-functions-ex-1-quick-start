ff3ce89e713e83afca4bee5629e6b16cfab7d944
rl627 <rl627@drexel.edu>
2022-06-21 13:37:36 -0400

    Modify HttpTrigger1: return JSON object

--- HttpTrigger1/index.js
+++ HttpTrigger1/index.js
@@ -1,13 +1,38 @@
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+   try {
+     context.log('JavaScript HTTP trigger function processed a request.');
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+     // Read incoming data
+     const name = req.query.name || (req.body && req.body.name);
+     const sport = req.query.sport || (req.body && req.body.sport);
 
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
+     // fail if incoming data is required
+     if (!name || !sport) {
+       context.res = {
+         status: 400,
+       };
+       return;
+     }
+
+     // Add or change code here
+     const message = `${name} likes ${sport}`;
+
+     // Construct response
+     const responseJSON = {
+       name: name,
+       sport: sport,
+       message: message,
+       success: true,
+     };
+
+     context.res = {
+       // status: 200, /* Defaults to 200 */
+       body: responseJSON,
+       contentType: 'application/json',
+     };
+   } catch (err) {
+     context.res = {
+       status: 500,
+     };
+   }
 }
